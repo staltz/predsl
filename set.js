a244fb7a5959f37632063fe3991f70bb40aadc35
Andre Staltz <andre@staltz.com>
2023-03-20 16:04:01 +0200

    init

---
+++ set.js
@@ -0,0 +1,137 @@
+function init(ssb, config) {
+  let oldestSeq = 1
+  let latestSeq = 0
+  const seqAdded = new Map()
+
+  const loadPromise = new Promise((resolve, reject) => {
+    const iterator = ssb.db.filterAsIterator(
+      (msg) => msg?.value?.content?.structure === 'set'
+    )
+    for (const msg of iterator) {
+      if (!msg) continue
+      if (!msg.value.content) continue
+      if (msg.value.content.oldest) {
+        oldestSeq = Math.max(oldestSeq, msg.value.content.oldest)
+      }
+      latestSeq = Math.max(latestSeq, msg.value.sequence)
+      if (msg.value.content.add) {
+        seqAdded.set(msg.value.content.add, msg.value.sequence)
+      }
+    }
+    resolve()
+  })
+
+  function loaded(cb) {
+    if (cb === void 0) return loadPromise
+    else loadPromise.then(() => cb(null), cb)
+  }
+
+  function has(name, cb) {
+    const iterator = ssb.db.filterAsIterator(
+      (msg) => msg?.value?.content?.structure === 'set'
+    )
+    let found = false
+    for (const msg of iterator) {
+      if (msg.value.content.add === name) {
+        found = true
+      } else if (msg.value.content.remove === name) {
+        found = false
+      }
+    }
+    return cb(null, found)
+  }
+
+  function add(name, cb) {
+    has(name, (err, exists) => {
+      if (err) return cb(err)
+      if (exists) return cb(null, false)
+      const newOldestSeq = seqAdded.size === 0 ? latestSeq + 1 : oldestSeq
+
+      ssb.db.create(
+        {
+          feedFormat: 'classic',
+          content: {
+            type: 'TODO',
+            structure: 'set',
+            oldest: newOldestSeq,
+            add: name,
+          },
+        },
+        (err, msg) => {
+          if (err) return cb(err)
+          seqAdded.set(name, msg.value.sequence)
+          latestSeq = msg.value.sequence
+          oldestSeq = newOldestSeq
+          cb(null, true)
+        }
+      )
+    })
+  }
+
+  function del(name, cb) {
+    has(name, (err, exists) => {
+      if (err) return cb(err)
+      if (!exists) return cb(null, false)
+      const seq = seqAdded.get(name)
+      const willBecomeEmpty = seqAdded.size === 1 && seqAdded.has(name)
+      seqAdded.delete(name)
+      const minSeqOfRemaining = Math.min(...seqAdded.values())
+      const newOldest = willBecomeEmpty
+        ? latestSeq + 1
+        : seq < minSeqOfRemaining
+        ? minSeqOfRemaining
+        : oldestSeq
+
+      ssb.db.create(
+        {
+          feedFormat: 'classic',
+          content: {
+            type: 'TODO',
+            structure: 'set',
+            oldest: newOldest,
+            remove: name,
+          },
+        },
+        (err, msg) => {
+          if (err) {
+            seqAdded.set(name, seq) // undo
+            return cb(err)
+          }
+          latestSeq = msg.value.sequence
+          oldestSeq = newOldest
+          cb(null, true)
+        }
+      )
+    })
+  }
+
+  function values() {
+    const iterator = ssb.db.filterAsIterator(
+      (msg) => msg?.value?.content?.structure === 'set'
+    )
+    const set = new Set()
+    for (const msg of iterator) {
+      const { content } = msg.value
+      if (content.add) set.add(content.add)
+      else if (content.remove) set.delete(content.remove)
+    }
+    return set.values()
+  }
+
+  return {
+    // Public API
+    loaded,
+    add,
+    has,
+    del,
+    values,
+
+    // Internal API, exposed for testing
+    _getOldest: () => oldestSeq,
+  }
+}
+
+module.exports = {
+  name: 'predsl',
+  init,
+}
---
+++ test/set.test.js
@@ -0,0 +1,81 @@
+const test = require('tape')
+const ssbKeys = require('ssb-keys')
+const path = require('path')
+const os = require('os')
+const rimraf = require('rimraf')
+const SecretStack = require('secret-stack')
+const caps = require('ssb-caps')
+const p = require('util').promisify
+
+const DIR = path.join(os.tmpdir(), 'ssb-predsl-set')
+rimraf.sync(DIR)
+
+let ssb
+test('setup', async (t) => {
+  ssb = SecretStack({ appKey: caps.shs })
+    .use(require('ssb-memdb'))
+    .use(require('ssb-classic'))
+    .use(require('ssb-box'))
+    .use(require('../set'))
+    .call(null, {
+      keys: ssbKeys.generate('ed25519', 'alice'),
+      path: DIR,
+    })
+
+  await ssb.db.loaded()
+  await ssb.predsl.loaded()
+})
+
+test('predsl Set add', async (t) => {
+  t.equals(ssb.predsl._getOldest(), 1, 'oldest=1')
+
+  t.ok(await p(ssb.predsl.add)('1st'), 'add 1st') // msg seq 1
+  t.equals(ssb.predsl._getOldest(), 1, 'oldest=1')
+
+  t.ok(await p(ssb.predsl.add)('2nd'), 'add 2nd') // msg seq 2
+  t.equals(ssb.predsl._getOldest(), 1, 'oldest=1')
+
+  t.true(await p(ssb.predsl.has)('1st'), 'has 1st')
+  t.ok(await p(ssb.predsl.del)('1st'), 'del 1st') // msg seq 3
+  t.equals(ssb.predsl._getOldest(), 2, 'oldest=2')
+
+  t.false(await p(ssb.predsl.has)('1st'), 'has not 1st')
+  t.ok(await p(ssb.predsl.del)('2nd'), 'del 2nd') // msg seq 4
+  t.equals(ssb.predsl._getOldest(), 4, 'oldest=4')
+
+  t.notOk(await p(ssb.predsl.del)('2nd'), 'del 2nd redundant')
+  t.false(await p(ssb.predsl.has)('2nd'), 'has not 2nd')
+  t.equals(ssb.predsl._getOldest(), 4, 'oldest=4')
+
+})
+
+test('predsl Set values iterator', async (t) => {
+  t.ok(await p(ssb.predsl.add)('alice'), 'add') // msg seq 5
+  t.equals(ssb.predsl._getOldest(), 5, 'oldest=5')
+
+  t.ok(await p(ssb.predsl.add)('bob'), 'add') // msg seq 6
+  t.equals(ssb.predsl._getOldest(), 5, 'oldest=5')
+
+  t.ok(await p(ssb.predsl.add)('carol'), 'add') // msg seq 7
+  t.equals(ssb.predsl._getOldest(), 5, 'oldest=5')
+
+  const expected = ['alice', 'bob', 'carol']
+  for (const member of ssb.predsl.values()) {
+    t.equal(member, expected.shift(), 'member')
+  }
+  t.equals(expected.length, 0, 'all members')
+})
+
+test('predsl Set delete mid does not change oldest', async (t) => {
+  t.ok(await p(ssb.predsl.del)('bob'), 'del bob') // msg seq 8
+  t.equals(ssb.predsl._getOldest(), 5, 'oldest=5')
+})
+
+test('predsl Sed delete oldest does change oldest', async (t) => {
+  t.ok(await p(ssb.predsl.del)('alice'), 'del alice') // msg seq 7
+  t.equals(ssb.predsl._getOldest(), 7, 'oldest=7')
+})
+
+test('teardown', (t) => {
+  ssb.close(t.end)
+})
